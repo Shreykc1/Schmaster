3c42ef4dbe1c74288e932eb9ec055de71a1342a2
Shrey Chandpa <shreykc1@gmail.com>
2024-08-10 08:38:29 +0100

    sws

--- schmaster-client/src/main.tsx
+++ schmaster-client/src/main.tsx
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./src/sw.js').then((registration) => {
+    navigator.serviceWorker.register('./sw.js').then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
       console.error('Service Worker registration failed:', error);
